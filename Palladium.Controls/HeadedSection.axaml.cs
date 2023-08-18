c61028e6b8a29983a7abe137c6cf664da992b6ac
Raphaël Buquet <22956744+RaphaelBuquet@users.noreply.github.com>
2023-08-18 18:25:11 +0100

    Add HeadedSection

---
+++ Palladium.Controls/HeadedSection.axaml.cs
@@ -0,0 +1,18 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using Avalonia.Metadata;
+
+namespace Palladium.Controls;
+
+public class HeadedSection : TemplatedControl
+{
+	public static readonly StyledProperty<Control?> ChildProperty = Decorator.ChildProperty.AddOwner<HeadedSection>();
+
+	[Content]
+	public Control? Child
+	{
+		get => GetValue(ChildProperty);
+		set => SetValue(ChildProperty, value);
+	}
+}
