1d2da748b6b5916254af1f131964f30294228c2f
RaphaelBuquet <22956744+RaphaelBuquet@users.noreply.github.com>
2023-08-18 09:56:35 +0100

    Start work on SearchBar control

---
+++ Palladium.Controls/SearchBar.axaml.cs
@@ -0,0 +1,17 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+
+namespace Palladium.Controls;
+
+public class SearchBar : TemplatedControl
+{
+	public static StyledProperty<string?> WatermarkProperty = TextBox.WatermarkProperty.AddOwner<SearchBar>();
+
+	public string? Watermark
+	{
+		get => GetValue(WatermarkProperty);
+		set => SetValue(WatermarkProperty, value);
+	}
+	
+}
