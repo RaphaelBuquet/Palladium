bc52ff8ed5abef15a198c3adc4963cadf463e171
Raphaël Buquet <22956744+RaphaelBuquet@users.noreply.github.com>
2023-08-18 11:12:56 +0100

    Further improve SearchBar

--- Palladium.Controls/SearchBar.axaml.cs
+++ Palladium.Controls/SearchBar.axaml.cs
@@ -6,12 +6,20 @@ namespace Palladium.Controls;
 
 public class SearchBar : TemplatedControl
 {
-	public static StyledProperty<string?> WatermarkProperty = TextBox.WatermarkProperty.AddOwner<SearchBar>();
+	public static readonly StyledProperty<string?> WatermarkProperty = TextBox.WatermarkProperty.AddOwner<SearchBar>();
+	public static readonly StyledProperty<string?> TextProperty = TextBox.TextProperty.AddOwner<SearchBar>();
+	
 
 	public string? Watermark
 	{
 		get => GetValue(WatermarkProperty);
 		set => SetValue(WatermarkProperty, value);
+	}
+	
+	public string? Text
+	{
+		get => GetValue(TextProperty);
+		set => SetValue(TextProperty, value);
 	}
 	
 }
