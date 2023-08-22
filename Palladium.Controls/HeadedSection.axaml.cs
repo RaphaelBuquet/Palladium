ace60afe745a597d991613d03519a6bc38a148a3
Raphaël Buquet <22956744+RaphaelBuquet@users.noreply.github.com>
2023-08-22 17:50:44 +0100

    Fix HeadedSection not showing contents in logical tree

--- Palladium.Controls/HeadedSection.axaml.cs
+++ Palladium.Controls/HeadedSection.axaml.cs
@@ -1,18 +1,6 @@
-﻿using Avalonia;
-using Avalonia.Controls;
-using Avalonia.Controls.Primitives;
-using Avalonia.Metadata;
+﻿using Avalonia.Controls;
 
 namespace Palladium.Controls;
 
-public class HeadedSection : TemplatedControl
-{
-	public static readonly StyledProperty<Control?> ChildProperty = Decorator.ChildProperty.AddOwner<HeadedSection>();
-
-	[Content]
-	public Control? Child
-	{
-		get => GetValue(ChildProperty);
-		set => SetValue(ChildProperty, value);
-	}
-}
+public class HeadedSection : ContentControl
+{ }
