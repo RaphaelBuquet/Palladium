a1b79c0b4d5fc2cbef1763be4cde67e6763333cf
Raphaël Buquet <22956744+RaphaelBuquet@users.noreply.github.com>
2023-10-01 18:01:29 +0100

    Add ActivityIndicator

---
+++ AppSource/Palladium.Controls/ActivityIndicator.axaml.cs
@@ -0,0 +1,8 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+
+namespace Palladium.Controls;
+
+public class ActivityIndicator : TemplatedControl
+{ }
---
+++ AppSource/Palladium.Controls/Resources/StepEasing.cs
@@ -0,0 +1,12 @@
+﻿using Avalonia.Animation.Easings;
+
+namespace Palladium.Controls.Resources;
+
+public class StepEasing : Easing
+{
+	/// <inheritdoc />
+	public override double Ease(double progress)
+	{
+		return Math.Floor(progress * 8.0) / 8.0;
+	}
+}
